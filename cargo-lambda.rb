e9b5e300e3ce185f3ebe140a566d966d562a42a3
David Calavera <david.calavera@gmail.com>
2022-05-21 19:48:02 -0700

    Release version 0.8.2
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- cargo-lambda.rb
+++ cargo-lambda.rb
@@ -3,11 +3,11 @@ class CargoLambda < Formula
   homepage "https://github.com/cargo-lambda/cargo-lambda"
 
   if OS.mac?
-    url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v0.8.0/cargo-lambda-v0.8.0-apple-darwin.tar.gz"
-    sha256 "823404b612c65d6a22539aa4cda147f24b392b698eaf1dd40f93f07d4a768da3"
+    url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v0.8.2/cargo-lambda-v0.8.2.apple-darwin.tar.gz"
+    sha256 "19493dbc01c8affc387b6199580260971f37cdcb020e2f09930b582e2a0cfa5b"
   else
-    url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v0.8.0/cargo-lambda-v0.8.0-i686-unknown-linux-musl.tar.gz"
-    sha256 "15fe7d5ce2ac55f40438ed200f2d9ce87909b69d6c472cda321e201330d42717"
+    url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v0.8.2/cargo-lambda-v0.8.2.x86_64-unknown-linux-musl.tar.gz"
+    sha256 "a792d672838608e8267ca6ca9fc25c99eb19ed7c6ec29756341471f1ca88c3b3"
   end
   license "MIT"
 
