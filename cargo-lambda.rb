15f0991632d2c589636f07039a4aef05f33af304
David Calavera <david.calavera@gmail.com>
2024-02-18 19:06:28 -0800

    Release v1.1.0
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- cargo-lambda.rb
+++ cargo-lambda.rb
@@ -1,15 +1,26 @@
 class CargoLambda < Formula
-  desc "cargo-lambda is a Cargo subcommand to help you work with AWS Lambda."
-  homepage "https://github.com/cargo-lambda/cargo-lambda"
+  desc "Cargo Lambda is a Cargo subcommand to help you work with AWS Lambda."
+  homepage "https://www.cargo-lambda.info"
+  head "https://github.com/cargo-lambda/cargo-lambda.git", branch: "main"
+  license "MIT"
 
   if OS.mac?
-    url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.0.3/cargo-lambda-v1.0.3.apple-darwin.tar.gz"
-    sha256 "9eec4204653246f81a41a456fb0964497f23c921da35f1b887eed2ec113f1cca"
+    if Hardware::CPU.arm?
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.1.0/cargo-lambda-v1.1.0.aarch64-apple-darwin.tar.gz"
+      sha256 "84482b938720ee414ac928ddddfadedbd324ee2b51a86fc3d9e2e5e8e97be46b"
+    else
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.1.0/cargo-lambda-v1.1.0.x86_64-apple-darwin.tar.gz"
+      sha256 "23a694f674afd9cf73b69cd80298ca6aa170ecff9eb359a2318345f9139e22b3"
+    end
   else
-    url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.0.3/cargo-lambda-v1.0.3.x86_64-unknown-linux-musl.tar.gz"
-    sha256 "dd51eb85c541663238b42f3ba50b97c9cf896509880377272a538b55d31530cd"
+    if Hardware::CPU.arm?
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.1.0/cargo-lambda-v1.1.0.aarch64-unknown-linux-musl.tar.gz"
+      sha256 "1e31107167c28e3e16fb7a93eba5798a36e81157513136a5604654c017ffc0e2"
+    else
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.1.0/cargo-lambda-v1.1.0.x86_64-unknown-linux-musl.tar.gz"
+      sha256 "329024740eb33f10a0d0cc795956d609081e557901803cdaf2963473f0b9dad7"
+    end
   end
-  license "MIT"
 
   depends_on "zig"
 
