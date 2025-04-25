787b2c33d029a397d7bc63def286ff7a0960aacf
David Calavera <david.calavera@gmail.com>
2025-04-25 16:05:39 -0700

    Release v1.8.4
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- cargo-lambda.rb
+++ cargo-lambda.rb
@@ -6,19 +6,19 @@ class CargoLambda < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.3/cargo-lambda-v1.8.3.aarch64-apple-darwin.tar.gz"
-      sha256 "83edbbba7e7bd2cd2d4a255fb6a356f70e1dc3d80c073a9ea1895d771838a35e"
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.4/cargo-lambda-v1.8.4.aarch64-apple-darwin.tar.gz"
+      sha256 "db6006e02ed008ed298e267d9407e08d3c92f207b6a8be24d997bbd98540e203"
     else
-      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.3/cargo-lambda-v1.8.3.x86_64-apple-darwin.tar.gz"
-      sha256 "1155e540037549ee98dc2e065563895fbefa92135bffc8be305bd26a7dd31704"
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.4/cargo-lambda-v1.8.4.x86_64-apple-darwin.tar.gz"
+      sha256 "0fed37fa41e680ffbc12606f7c93ae7813d7866a53d264ce5f5986b884c8f2ac"
     end
   else
     if Hardware::CPU.arm?
-      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.3/cargo-lambda-v1.8.3.aarch64-unknown-linux-musl.tar.gz"
-      sha256 "c4b808f777a7bffe7a7851ef98a609576a621c7fecea690fd2077eb25c3f01ee"
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.4/cargo-lambda-v1.8.4.aarch64-unknown-linux-musl.tar.gz"
+      sha256 "d6b5b7cbb6d6f0b4be7f2be3cab2598a60ea9c58a0f80496d9cb0e020511b128"
     else
-      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.3/cargo-lambda-v1.8.3.x86_64-unknown-linux-musl.tar.gz"
-      sha256 "9ffe7b8f6ffaad73484be4fd2e5c4365e3992a0ff074d11df9c0d2895a28cff4"
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.4/cargo-lambda-v1.8.4.x86_64-unknown-linux-musl.tar.gz"
+      sha256 "97a743fce803b331cc1e38d9a05cd2455233b24d697798448d0d00ec53c45074"
     end
   end
 
