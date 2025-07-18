3f70884c53d7dfb135d049d7e3edabf318d983a7
David Calavera <1050+calavera@users.noreply.github.com>
2025-07-17 18:33:44 -0700

    Release v1.8.6
    Signed-off-by: David Calavera <1050+calavera@users.noreply.github.com>

--- cargo-lambda.rb
+++ cargo-lambda.rb
@@ -6,19 +6,19 @@ class CargoLambda < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.5/cargo-lambda-v1.8.5.aarch64-apple-darwin.tar.gz"
-      sha256 "88847c5c24c15762fcc657cb4d8a809535361e361dc1e159733377f49692f86a"
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.6/cargo-lambda-v1.8.6.aarch64-apple-darwin.tar.gz"
+      sha256 "bc73b657c0cdfe15badca1b2cb485df742dd9d16440dbff17ba0a29655c27c79"
     else
-      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.5/cargo-lambda-v1.8.5.x86_64-apple-darwin.tar.gz"
-      sha256 "813aca0784da967ce8745953717e38ad2e512999c276d0b6764c9092f4a7a5ac"
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.6/cargo-lambda-v1.8.6.x86_64-apple-darwin.tar.gz"
+      sha256 "a43e2dd0d3626d7c7253978801104b6fc8688d27dd2f743d19ba769168014fd6"
     end
   else
     if Hardware::CPU.arm?
-      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.5/cargo-lambda-v1.8.5.aarch64-unknown-linux-musl.tar.gz"
-      sha256 "c68117e4669724a813f0d30e923869cb11d52d1d31b0452fc81662be474c5c9e"
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.6/cargo-lambda-v1.8.6.aarch64-unknown-linux-musl.tar.gz"
+      sha256 "796f96196f3ba14a4a2542cf7f7dd480eb556f9fa5f04653a1ad4dffc5044567"
     else
-      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.5/cargo-lambda-v1.8.5.x86_64-unknown-linux-musl.tar.gz"
-      sha256 "c85abcef098b2add55fcbf44b1287663bae26773dd1e900fb2d99b1f8d8453af"
+      url "https://github.com/cargo-lambda/cargo-lambda/releases/download/v1.8.6/cargo-lambda-v1.8.6.x86_64-unknown-linux-musl.tar.gz"
+      sha256 "717261a71adad7fdae3056882d2422fc9ecf9232eea9e5aaf8130d1cbb9c76e6"
     end
   end
 
